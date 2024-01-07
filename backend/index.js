9277d2f1b7e617452374123964041b9767b5f39a
Shaswat Deep <deepshaswat@gmail.com>
2024-01-07 11:16:43 +0530

    New year stream

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+// write basic express boilerplate code
+// with express.json() middleware
+
+const express = require("express");
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+
+/*
+body {
+    title: string;
+    description: string
+}
+*/
+
+app.post("/todo", (req, res) => {});
+
+app.get("/todo", (req, res) => {});
+
+app.post("/completed", (req, res) => {});
